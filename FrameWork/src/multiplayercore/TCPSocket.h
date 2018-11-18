f6589f7024b43c4b2a6db7a112596b82cb984e8d
CoheeStudio <alwayssmile11a1@gmail.com>
2018-11-18 23:33:21 +0700

    TCP and UDP

---
+++ FrameWork/src/multiplayercore/TCPSocket.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "SocketAddress.h"
+#include "SocketUtils.h"
+class TCPSocket
+{
+public:
+	~TCPSocket();
+	int								Connect(const SocketAddress& inAddress);
+	int								Bind(const SocketAddress& inToAddress);
+	int								Listen(int inBackLog = 32);
+	std::shared_ptr< TCPSocket >			Accept(SocketAddress& inFromAddress);
+	int32_t							Send(const void* inData, size_t inLen);
+	int32_t							Receive(void* inBuffer, size_t inLen);
+private:
+	friend class SocketUtil;
+	TCPSocket(SOCKET inSocket) : mSocket(inSocket) {}
+	SOCKET		mSocket;
+};
+typedef std::shared_ptr< TCPSocket > TCPSocketPtr;
--- FrameWork/src/multiplayercore/UDPSocket.h
+++ FrameWork/src/multiplayercore/UDPSocket.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "SocketAddress.h"
+#include "SocketUtils.h"
 
 class UDPSocket
 {
